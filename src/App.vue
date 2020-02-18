dbac3ade5aab9cb5ae8c8539b6d2502ac8a9dce2
atcha <francois.cornuaud@mutuelledepoitiers.fr>
2020-02-18 17:34:56 +0100

    UPD - properties on card component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
-    <div id="app" class="container mx-auto flex">
-        <user-card class="lg:w-1/2"></user-card>
+    <div id="app" class="container mx-auto flex flex-col items-center">
+        <h1>Liste des participants aux Human Talks Poitiers</h1>
+        <card class="lg:w-1/2"
+              name="Erin Lindford"
+              role="Customer Support"
+              mail="erinlindford@example.com"
+              phone="(555) 765-4321"></card>
     </div>
 </template>
 
 <script>
-    import UserCard from './components/UserCard'
+    import Card from './components/Card';
+
 
     export default {
         name: 'App',
         components: {
-            UserCard
+            Card
         },
-        
+        mounted() {
+            this.fetchData();
+        },
+        data() {
+            return {
+                siteUrl: "https://www.meetup.com/fr-FR/Human-Talks-Poitiers/members/",
+                siteResult: {}
+            }
+        },
+        methods: {
+            fetchData() {
+                fetch("https://www.meetup.com/fr-FR/Human-Talks-Poitiers/members/",
+                    {
+                        method: 'GET',
+                        mode: 'no-cors'
+                    })
+                    .then((response) => {
+                        return response.text();
+                        // cheerio.load(result.data);
+                    }).then((html) => {
+                        console.log(html);
+                    });
+
+            }
+        }
     }
 </script>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,17 +2,18 @@
     <div class="md:flex bg-white rounded-lg p-6 shadow">
         <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="../assets/avatar.svg">
         <div class="text-center md:text-left">
-            <h2 class="text-lg">Erin Lindford</h2>
-            <div class="text-purple-500">Customer Support</div>
-            <div class="text-gray-600">erinlindford@example.com</div>
-            <div class="text-gray-600">(555) 765-4321</div>
+            <h2 class="text-lg">{{ name }}</h2>
+            <div class="text-purple-500">{{ role }}</div>
+            <div class="text-gray-600">{{ mail }}</div>
+            <div class="text-gray-600">{{ phone }}</div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "UserCard"
+        name: "Card",
+        props: ["name", "role", "mail", "phone"]
     }
 </script>
 
