f76aa214091b41cd51aca00bfb55020f383c5948
atcha <francois.cornuaud@gmail.com>
2020-02-20 18:46:12 +0100

    UPD - image size correct

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,7 +2,7 @@
     <div class="fixed z-30 inset-y-0 left-0 w-64 px-8 py-4 bg-gray-100 border-r overflow-auto lg:static lg:inset-auto transform lg:translate-x-0 transition-medium"
          :class="[sideBarIsOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in']">
         <div class="-mx-3 pl-3 pr-1 flex items-center justify-between">
-            <span><img alt="" src="../assets/logo.png" class="h-9 w-9"></span>
+            <span><img alt="" src="../assets/logo.png" class="h-16 w-auto"></span>
             <button class="text-gray-700 lg:hidden" @click="closeSideBar">
                 <svg fill="none" viewBox="0 0 24 24" class="h-6 w-6">
                     <path d="M6 18L18 6M6 6L18 18" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
